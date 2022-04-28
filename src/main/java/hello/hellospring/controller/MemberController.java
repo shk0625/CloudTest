a590c8d3741796d58cb2851c03ac4c406e264881
yohan <noyohanx@naver.com>
2022-04-28 14:51:33 +0900

    :white_check_mark: : 자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+    //@Autowired
+    private MemberService memberService;
 
+    // 생성자 주입을 쓰는게 베스트이다.
+    // 의존관계가 실행중에 동적으로 변하는 경우는 거의 없기 때문.
+    // + 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용한다.
+    // 정형화 되지 않거나 상황에 따라 구현 클래스를 변경해야하면 설정을 통해 스프링 빈으로 등록한다.
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,12 +9,9 @@ import org.springframework.stereotype.Service;
 import javax.websocket.server.ServerEndpoint;
 import java.util.List;
 import java.util.Optional;
-
-@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
